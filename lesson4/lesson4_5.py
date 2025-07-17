2db11a5da04024d0cba0bae343c6a48c8e6ae0e5
yh123123123 <yunghsu123@yahoo.com.tw>
2025-07-17 16:52:15 +0800

    20250717

---
+++ lesson4/lesson4_5.py
@@ -0,0 +1,16 @@
+from playwright.async_api import async_playwright
+import asyncio
+
+async def main():
+    async with async_playwright() as p:
+        #browser = await p.chromium.launch(headless=False)
+        browser = await p.firefox.launch(headless=False,slow_mo=1000)  # firefox
+        page = await browser.new_page()
+        await page.goto('https://example.com')
+        await page.wait_for_selector('p') #等待元素載入
+        content = await page.inner_text('p')
+        print(content)
+        await browser.close()
+    
+asyncio.run(main())
+
